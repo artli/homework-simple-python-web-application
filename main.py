f619e9953f928afa780bfc9f987305125bee7e69
Arthur Liss <artli@e1.ru>
2015-04-14 02:33:18 +0500

    Copy Bottle documentation example

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 from __future__ import unicode_literals, print_function, generators, division
-
-__author__ = 'pahaz'
-
-
-def application(environ, start_response):
-    assert environ.get('PATH_INFO') is not None, "environ['PATH_INFO'] is None"
-
-    status = "200 OK"
-    headers = [('Content-type', 'text/html; charset=utf-8')]
-    body = """<!DOCTYPE html>
-    <h1>Example-mini-application</h1>
-    """
-
-    start_response(status, headers)
-    return [body.encode('utf-8')]
+from bottle import route, run, template
 
 
-if __name__ == "__main__":
-    from wsgiref.simple_server import make_server
-    PORT = 31338
-    print("It's work! Visit http://localhost:{0}/".format(PORT))
-    HTTPD = make_server('', PORT, application)
-    HTTPD.serve_forever()
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+
+run(host='localhost', port=8080)
