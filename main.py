0b9a8790c7563d077204e7e02f2f80201aade7d4
Arthur Liss <artli@e1.ru>
2015-04-14 15:09:17 +0500

    Set URL schema

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from __future__ import unicode_literals, print_function, generators, division
-from bottle import route, run, template
+from bottle import Bottle, run, static_file
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+app = Bottle()
 
 
-run(host='localhost', port=8080)
+@app.get('/')
+def show_index():
+    return 'Main page; form for adding a link; list of all links'
+
+
+@app.post('/')
+def add_link():
+    return 'Add a link and redirect to main page'
+
+
+@app.get('/<link_id:re:[^-/]+>')
+def follow_link(link_id):
+    return 'Redirect to wherever link "{}" points'.format(link_id)
+
+
+@app.get('/<link_id:re:[^-/]+->')
+def show_link_stat(link_id):
+    return 'Show stat for link "{}"'.format(link_id)
+
+
+@app.get('/static/<filepath:path>')
+def send_static_file(filepath):
+    return static_file(filepath, '/static')
+
+
+run(app, host='localhost', port=8080, debug=True)
